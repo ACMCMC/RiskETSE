7d7d8b03789027751028bc1769fd05b335763287
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-10-07 12:38:35 +0200

    Cogido el formato del CV

---
+++ test/risk/MenuTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package risk;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author aldan.creo
+ */
+public class MenuTest {
+    
+    public MenuTest() {
+    }
+
+    @Test
+    public void testAsignarPaises_File() {
+    }
+
+    @Test
+    public void testAsignarPaises_String_String() {
+    }
+
+    @Test
+    public void testCrearMapa() {
+    }
+    
+}
