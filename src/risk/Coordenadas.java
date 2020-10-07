197fdd450bc559412208ad4876fdd0e50b61c28b
Aldán Creo <20495460+ACMCMC@users.noreply.github.com>
2020-10-07 11:11:56 +0000

    Creadas clases nuevas

---
+++ src/risk/Coordenadas.java
@@ -0,0 +1,21 @@
+package risk;
+
+public class Coordenadas {
+    
+    private int X;
+    private int Y;
+    
+    Coordenadas (int X, int Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+    
+    public int getX() {
+        return this.X;
+    }
+
+    public int getY() {
+        return this.Y;
+    }
+    
+}
