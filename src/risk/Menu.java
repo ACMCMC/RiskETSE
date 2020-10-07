9f76176dce80c1900601ee0bfecdc3ec3ecb5e3b
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2020-10-07 12:32:15 +0200

    Cogido el formato del CV

--- src/risk/Menu.java
+++ src/risk/Menu.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -16,26 +14,23 @@ public class Menu {
     // En esta clase se deberían de definir los atributos a los que será 
     // necesario acceder durante la ejecución del programa como, por ejemplo,
     // el mapa o los jugadores
-    
+
     static final Logger logger = Logger.getLogger(Menu.class.getCanonicalName());
-    
-    /**
-     * 
-     */
+
     public Menu() {
         // Inicialización de algunos atributos
-        
+
         // Iniciar juego
-        String orden= null;
-        BufferedReader bufferLector= null;
+        String orden = null;
+        BufferedReader bufferLector = null;
         try {
-            File fichero=  new File("comandos.csv");
-            FileReader lector= new FileReader(fichero);
-            bufferLector= new BufferedReader(lector);
-            while((orden= bufferLector.readLine())!=null) {
+            File fichero = new File("comandos.csv");
+            FileReader lector = new FileReader(fichero);
+            bufferLector = new BufferedReader(lector);
+            while ((orden = bufferLector.readLine()) != null) {
                 System.out.println("$> " + orden);
-                String[] partes=orden.split(" ");
-                String comando= partes[0];
+                String[] partes = orden.split(" ");
+                String comando = partes[0];
                 // COMANDOS INICIALES PARA EMPEZAR A JUGAR
                 //    crear mapa
                 //    crear jugadores <nombre_fichero>
@@ -56,10 +51,10 @@ public class Menu {
                 //    repartir ejercitos
                 //    ver mapa
                 //    ver pais <nombre_pais>
-                switch(comando) {
+                switch (comando) {
                     case "crear":
-                        if(partes.length==2) {
-                            if(partes[1].equals("mapa")) {
+                        if (partes.length == 2) {
+                            if (partes[1].equals("mapa")) {
                                 // crearMapa es un método de la clase Menú desde el que se puede invocar
                                 // a otros métodos de las clases que contienen los atributos y los métodos
                                 // necesarios para realizar esa invocación 
@@ -67,9 +62,10 @@ public class Menu {
                             } else {
                                 System.out.println("\nComando incorrecto.");
                             }
-                        } if(partes.length==3) {
-                            if(partes[1].equals("jugadores")) { 
-                                try {    
+                        }
+                        if (partes.length == 3) {
+                            if (partes[1].equals("jugadores")) {
+                                try {
                                     crearJugador(new File(partes[2]));
                                 } catch (FileNotFoundException ex) {
                                     System.out.println("No existe el archivo especificado.");
@@ -83,9 +79,9 @@ public class Menu {
                         }
                         break;
                     case "asignar":
-                        if(partes.length!=3) {
+                        if (partes.length != 3) {
                             System.out.println("\nComando incorrecto.");
-                        } else if(partes[1].equals("paises")) {
+                        } else if (partes[1].equals("paises")) {
                             // asignarPaises es un método de la clase Menu que recibe como entrada el fichero
                             // en el que se encuentra la asignación de países a jugadores. Dentro de este
                             // método se invocará a otros métodos de las clases que contienen los atributos
@@ -99,55 +95,53 @@ public class Menu {
                         System.out.println("\nComando incorrecto.");
                 }
             }
-        } catch(Exception excepcion) {
+        } catch (Exception excepcion) {
             excepcion.printStackTrace();
         }
     }
 
     /**
-     * 
-     * @param file 
+     *
+     * @param file
      */
     public void asignarPaises(File file) {
         // Código necesario para asignar países
     }
 
     /**
-     * 
+     *
      * @param nombrePais
-     * @param nombreJugador 
+     * @param nombreJugador
      */
     public void asignarPaises(String nombrePais, String nombreJugador) {
         // Código necesario para asignar un país a un jugador
     }
 
     /**
-     * 
+     *
      */
     public void crearMapa() {
         // Código necesario para crear el mapa
     }
-        
+
     /**
-     * 
-     * @param file 
+     *
+     * @param file
      */
     private void crearJugador(File file) throws FileNotFoundException {
         // Código necesario para crear a los jugadores del RISK
-        
         try {
-            FileReader lector= new FileReader(file);
-            BufferedReader bufferLector= new BufferedReader(lector);
+            FileReader lector = new FileReader(file);
+            BufferedReader bufferLector = new BufferedReader(lector);
         } catch (FileNotFoundException fileNotFoundException) {
             // Si no se encuentra el archivo, falla el programa
             throw fileNotFoundException;
         }
-
     }
-    
+
     /**
-     * 
-     * @param file 
+     *
+     * @param file
      */
     private void crearJugador(String nombre, String color) {
         // Código necesario para crear a un jugador a partir de su nombre y color
