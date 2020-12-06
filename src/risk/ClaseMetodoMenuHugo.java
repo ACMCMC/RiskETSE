d0297f0cdd3c00499a59008814df0e5d8890c223
hugogs02 <57640506+hugogs02@users.noreply.github.com>
2020-12-06 23:54:29 +0100

    Creada funcion comprobarSiComandoEsSintacticamenteCorrecto

--- src/risk/ClaseMetodoMenuHugo.java
+++ src/risk/ClaseMetodoMenuHugo.java
@@ -1,5 +1,8 @@
 package risk;
 
+import javax.lang.model.util.ElementScanner6;
+import javax.xml.namespace.QName;
+
 public class ClaseMetodoMenuHugo {
     public void Menu() {
         // METE AQUI COMENTARIOS QUE INDIQUEN QUE PASOS HAY QUE SEGUIR PARA HACER EL MENU
@@ -35,20 +38,177 @@ public class ClaseMetodoMenuHugo {
         //ademas, el jugador puede pasar tantos ejercitos como pueda de un pais a otro que le pertenezca, siempre que quede en el pais de origen minimo 1 ejercito. Esto se puede hacer una vez por turno
     }
 
-    /*public boolean comprobarSiComandoEsSintacticamenteCorrecto(String comando) {
+    public boolean comprobarSiComandoEsSintacticamenteCorrecto(String comando) {
         String partesComando[] = comando.split(" ");
         if (partesComando.length > 1) {
             switch (partesComando[0]){
             case "crear":
-            return true;
+                if(partesComando.length==2 && partesComando[1]=="mapa"){
+                    return true;
+                }
+                else if(partesComando.length==3){
+                    if(partesComando[1].equals("nombrejugador")){
+                        if(partesComando[2].equals("color")){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else if(partesComando[1].equals("jugadores")){
+                        if(partesComando[2].equals("nombrearchivo")){
+                            return true;
+                        } 
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+
+            case "obtener":
+                if(partesComando.length==3){
+                    if(partesComando[1].equals("frontera") && partesComando[2].equals("abrevPais")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("continente") && partesComando[2].equals("abrevPais")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("color") && partesComando[2].equals("abrevPais")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("paises") && partesComando[2].equals("abrevCont")){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+
+            case "repartir":
+                if(partesComando.length==2 && partesComando[1].equals("ejercitos")){
+                    return true;
+                }
+                else if(partesComando.length==4 && partesComando[1].equals("ejercitos") && partesComando[2].equals("numero") && partesComando[3].equals("nombrePais")){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+
             case "cambiar":
-                if (partesComando.length==2 && partesComando[1].equals("cartas"))
-                return true;
+                if (partesComando.length==3 && partesComando[1].equals("cartas") && partesComando[2].equals("todas")){
+                    return true;
+                }
+                else if(partesComando.length==5 && partesComando[1].equals("cartas") && partesComando[2].equals("id1") && partesComando[3].equals("id2") && partesComando[4].equals("id3")){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+
+            case "asignar":
+                if(partesComando.length==3){
+                    if(partesComando[1].equals("misiones") && partesComando[2].equals("nombrefichero")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("paises") && partesComando[2].equals("nombrefichero")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("cartas") && partesComando[2].equals("idcarta")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("nombrejugador") && partesComando[2].equals("idmision")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("nombrejugador") && partesComando[2].equals("nombrepais")){
+                        return false;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+
+            case "describir":
+                if(partesComando.length==3){
+                    if(partesComando[1].equals("jugador") && partesComando[2].equals("nombrejugador")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("pais") && partesComando[2].equals("nombrepais")){
+                        return true;
+                    }
+                    else if(partesComando[1].equals("continente") && partesComando[2].equals("nombrecontinente")){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+
+            case "atacar":
+                if(partesComando.length==3){
+                    if(partesComando[1].equals("abrevPais1") && partesComando[2].equals("abrevPais2")){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else if(partesComando.length==5){
+                    if(partesComando[1].equals("abrevPais1") && partesComando[2].equals("dadosAtaque") && partesComando[3].equals("abrevPais2") && partesComando[4].equals("dadosDefensa")){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+
+            case "rearmar":
+                if(partesComando.length==4 && partesComando[1].equals("abrevPais1") && partesComando[2].equals("numEjercitos") && partesComando[3].equals("abrevPais2")){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+
+            case "acabar":
+                if(partesComando.length==2 && partesComando[1].equals("turno"))
+                    return true;
                 else
-                return false;
+                    return false;
+
+            case "ver":
+                if(partesComando.length==2 && partesComando[1].equals("mapa"))
+                    return true;
+                else
+                    return false;
+
             default:
-            return false;
+                return false;
             }
         }
-    }*/
+        else if (partesComando.length==1 && partesComando[0]=="Jugador"){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
